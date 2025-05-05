655191b2fb0bf51c2e4ecbe3bc11aa386b4905a5
allcodebreaker <allwin2436@gmail.com>
2025-05-05 22:42:20 +0530

    Basic Code

---
+++ backend/static/js/main.js
@@ -0,0 +1,12 @@
+function sendOption(){
+    let sendLayout = document.getElementById("send-layout");
+    sendLayout.style.display = "block";
+
+}
+
+
+function receiveLayout(){
+    let receiveLayout = document.getElementById("receive-layout");
+    receiveLayout.style.display = "block";
+
+}
